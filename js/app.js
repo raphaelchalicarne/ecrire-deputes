d7335c9bcd98b1b0c35c39bf7a6368f28f6c3ccf
Raphaël Chalicarne <39592819+raphaelchalicarne@users.noreply.github.com>
2022-07-09 23:01:34 +0200

    :sparkles: Select by commision & groupe (#3)
    * :sparkles: Select by commision
    
    * :sparkles: Select by groupe

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 window.onload = function () {
-  populateEmailOptions();
+  const commission_select = document.getElementById('commission');
+  const groupe_select = document.getElementById('groupe');
+  const deputes_select = document.getElementById('deputes_emails');
+
+  populateCategoryOptions(commission_select, 'commission_permanente');
+  populateCategoryOptions(groupe_select, 'groupe_sigle');
+  populateEmailOptions(deputes_select);
+
+  commission_select.oninput = selectEmailsByCommission;
+  groupe_select.oninput = selectEmailsByGroupe;
+  deputes_select.oninput = unSelectOptions;
 };
 
 async function fetchData() {
@@ -7,15 +17,67 @@ async function fetchData() {
   return await export_deputes.json();
 }
 
-async function populateEmailOptions() {
-  var select_deputes = document.getElementById('deputes_emails');
+async function populateCategoryOptions(select_elt, key) {
+  const export_deputes_json = await fetchData();
+  [... new Set(export_deputes_json.map(depute => depute[key]))]
+    .forEach(commission => {
+      let opt = document.createElement("option");
+      opt.value = commission;
+      opt.label = commission;
+      select_elt.appendChild(opt);
+    });
+}
+
+async function populateEmailOptions(deputes_select) {
   const export_deputes_json = await fetchData();
   export_deputes_json.forEach(depute => {
     let opt = document.createElement("option");
     opt.value = depute["assemblee_nationale_email"];
     opt.label = depute["nom"];
-    select_deputes.appendChild(opt);
+    deputes_select.appendChild(opt);
   });
+}
+
+async function selectEmailsByCommission() {
+  const commission_select = document.getElementById('commission');
+  const deputes_select = document.getElementById('deputes_emails');
+
+  [...deputes_select.selectedOptions].forEach(o => o.selected = false);
+
+  let selected_commissions = [...commission_select.selectedOptions].map(o => o.value);
+  const export_deputes_json = await fetchData();
+
+  [...deputes_select.options].forEach((o) => {
+    let depute = export_deputes_json.find(d => d["assemblee_nationale_email"] == o.value);
+    if (selected_commissions.includes(depute["commission_permanente"])) {
+      o.selected = true;
+    }
+  });
+}
+
+async function selectEmailsByGroupe() {
+  const groupe_select = document.getElementById('groupe');
+  const deputes_select = document.getElementById('deputes_emails');
+
+  [...deputes_select.selectedOptions].forEach(o => o.selected = false);
+
+  let selected_commissions = [...groupe_select.selectedOptions].map(o => o.value);
+  const export_deputes_json = await fetchData();
+
+  [...deputes_select.options].forEach((o) => {
+    let depute = export_deputes_json.find(d => d["assemblee_nationale_email"] == o.value);
+    if (selected_commissions.includes(depute["groupe_sigle"])) {
+      o.selected = true;
+    }
+  });
+}
+
+function unSelectOptions() {
+  const commission_select = document.getElementById('commission');
+  const groupe_select = document.getElementById('groupe');
+
+  [...commission_select.selectedOptions].forEach(o => o.selected = false);
+  [...groupe_select.selectedOptions].forEach(o => o.selected = false);
 }
 
 function writeEmail() {
