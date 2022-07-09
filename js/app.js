4d71cb5dcff17cc4184abc680ca3bcaf29fc62bf
Raphaël Chalicarne <39592819+raphaelchalicarne@users.noreply.github.com>
2022-07-09 16:18:22 +0200

    :sparkles: Enable mailing list (#2)
    * :lipstick: Center h1
    
    * :see_no_evil: Add gitignore
    
    * :card_file_box: Include data in repo
    
    * :sparkles: Write multiple emails
    
    * :art:
    
    * :bug: Avoid error 405

--- js/app.js
+++ js/app.js
@@ -1,10 +1,32 @@
+window.onload = function () {
+  populateEmailOptions();
+};
+
+async function fetchData() {
+  const export_deputes = await fetch('/data/export_deputes.json')
+  return await export_deputes.json();
+}
+
+async function populateEmailOptions() {
+  var select_deputes = document.getElementById('deputes_emails');
+  const export_deputes_json = await fetchData();
+  export_deputes_json.forEach(depute => {
+    let opt = document.createElement("option");
+    opt.value = depute["assemblee_nationale_email"];
+    opt.label = depute["nom"];
+    select_deputes.appendChild(opt);
+  });
+}
+
 function writeEmail() {
   var form = document.forms.email_deputes;
-  let email_address = form.elements["inputEmail"].value;
+  var selected_deputes_emails = [...form.elements["deputes_emails"].selectedOptions]
+    .map(selected_depute => selected_depute.value)
+    .join();
   let email_subject = form.elements["inputObjet"].value;
   let email_body = form.elements["inputCorps"].value;
 
-  const mailto_url = new URL('mailto:' + email_address);
+  const mailto_url = new URL('mailto:' + selected_deputes_emails);
   mailto_url.searchParams.append('subject', email_subject);
   mailto_url.searchParams.append('body', email_body);
   window.open(mailto_url);
